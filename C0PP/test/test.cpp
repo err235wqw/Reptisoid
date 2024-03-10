aeefd4240a601573a88d8d24ce1f72f15d55d1c0
Вячеслав Синев <vyacheslav.sinew@yandex.ru>
2024-03-10 07:56:29 +0400

    Написал мини шаблон для проги

---
+++ C0PP/test/test.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+
+#include <numeric>
+#include <algorithm>
+
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+#define endl '\n'
+#define vi vector <int>
+
+
+void Solve() {
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout << setprecision(20) << fixed << '\n';
+
+	int tt;
+	cin >> tt;
+
+	for (int i = 0; i < tt; i++) {
+		Solve();
+	}
+	return 0;
+}
