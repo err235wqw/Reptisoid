8fd4bd199e5d62f367e0573c8d6c367562922204
Vyacheslav Sinew <vyacheslav.sinew@yandex.ru>
2024-09-12 02:02:03 +0400

    ...

--- MetVich_3_1/1_2.py
+++ MetVich_3_1/1_2.py
@@ -6,5 +6,5 @@ def main():
     for x in (arange(0.5, 2.5, 1)):
          answer = sum([prod([x - x_i for x_i in range(0,n) if x_i != x_iz]) / prod([x_iz - x_i for x_i in range(0,n) if x_i != x_iz]) for x_iz in range(0,n)])
          print(f"P_n({x}) = {answer}")
-
+         
 main()
