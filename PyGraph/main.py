9e729651dddc55807fc4fd7945af75be7b3a18d3
Vyacheslav Sinew <vyacheslav.sinew@yandex.ru>
2024-09-14 16:05:18 +0400

    хз

---
+++ PyGraph/main.py
@@ -0,0 +1,14 @@
+def main():
+    a, b = int(input()), int(input())
+    a1 = a
+    sum = a
+    cnt1 = 1
+    cnt2 = 1
+    while a1 < b:
+        a1*=3
+        cnt+=1
+    while sum < 1000000:
+        a*= 3
+        sum +=a
+
+main()
