21c86d785053d9d0e29ca192d5713ddcde693f9d
Vyacheslav Sinew <vyacheslav.sinew@yandex.ru>
2024-09-17 14:26:17 +0400

    Закинул 2ю методы вычислений

--- MetVich_3_1/1_2.py
+++ MetVich_3_1/1_2.py
@@ -3,8 +3,9 @@ from math import prod
 
 def main():
     variant, n = int(input()), int(input())
-    for x in (arange(0.5, 2.5, 1)):
-         answer = sum([prod([x - x_i for x_i in range(0,n) if x_i != x_iz]) / prod([x_iz - x_i for x_i in range(0,n) if x_i != x_iz]) for x_iz in range(0,n)])
+    y_i = [variant + i**3 for i in range(0,n+1)]
+    for x in (arange(0.5, 2.6, 1)):
+         answer = sum([(prod([x - x_i for x_i in range(0,n) if x_i != x_iz]) / prod([x_iz - x_i for x_i in range(0,n) if x_i != x_iz])) * y_i[x_iz] for x_iz in range(0,n)])
          print(f"P_n({x}) = {answer}")
          
 main()
